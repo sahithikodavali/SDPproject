3d710cce59b539c1f87a0f5339a5d1558070ac46
sahithikodavali <111578271+sahithikodavali@users.noreply.github.com>
2024-03-21 10:52:04 +0530

    your message

---
+++ threadapp/urls.py
@@ -0,0 +1,11 @@
+from .import views
+from.views import *
+from .views import edit_emp
+from django.urls import path
+
+urlpatterns = [
+    path('', views.insert_emp, name='insert-emp'),
+    path('show/', views.show_emp, name='show-emp'),
+    path('edit/<int:pk>', views.edit_emp, name='edit-emp'),
+    path('remove/<int:pk>', views.remove_emp, name='remove-emp'),
+]
